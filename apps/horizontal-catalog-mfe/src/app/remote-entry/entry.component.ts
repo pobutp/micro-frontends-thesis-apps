fc422ac0b0d570a844bb3470802612a5e34b2099
Przemek Pobuta <przemyslawpobuta@gmail.com>
2022-02-27 16:30:42 +0100

    clean up

--- apps/horizontal-catalog-mfe/src/app/remote-entry/entry.component.ts
+++ apps/horizontal-catalog-mfe/src/app/remote-entry/entry.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'micro-frontends-thesis-apps-horizontal-catalog-mfe-entry',
-  template: `<div class="remote-entry">
-    <h2>horizontal-catalog-mfe's Remote Entry Component</h2>
-  </div>`,
+  template: `
+    <div class="remote-entry">
+      <h2>horizontal-catalog-mfe's Remote Entry Component</h2>
+    </div>
+  `,
   styles: [
     `
       .remote-entry {
