00ccd336ef4a2e68f30e6f5545a5f237b51559a8
Przemek Pobuta <przemyslawpobuta@gmail.com>
2022-08-30 11:09:41 +0200

    final changes

--- apps/horizontal-shell/src/app/app.module.ts
+++ apps/horizontal-shell/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { startsWith } from '@micro-frontends-thesis-apps/shared';
 
 import { AppComponent } from './app.component';
@@ -10,26 +10,21 @@ import { IdentityComponent } from './components/identity/identity.component';
 import { OrdersAccountComponent } from './components/orders-account/orders-account.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
 
+export const routes: Routes = [
+  {
+    matcher: startsWith('catalog-basket'),
+    component: CatalogBasketComponent,
+  },
+  {
+    matcher: startsWith('orders-account'),
+    component: OrdersAccountComponent,
+  },
+  { path: '', redirectTo: '/catalog-basket', pathMatch: 'full' },
+];
+
 @NgModule({
   declarations: [AppComponent, WrapperComponent, OrdersAccountComponent, CatalogBasketComponent, IdentityComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(
-      [
-        {
-          matcher: startsWith('catalog-basket'),
-          component: CatalogBasketComponent,
-        },
-        {
-          matcher: startsWith('orders-account'),
-          component: OrdersAccountComponent,
-        },
-        { path: '', redirectTo: '/catalog-basket', pathMatch: 'full' },
-      ],
-      { relativeLinkResolution: 'legacy' }
-    ),
-  ],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   providers: [],
   bootstrap: [AppComponent],
 })
